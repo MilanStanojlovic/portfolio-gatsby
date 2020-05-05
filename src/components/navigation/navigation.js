8434c6e02c9e833e9f2718303b929c57c40f1eef
Milan <mistikone1@gmail.com>
2020-05-05 12:38:18 +0200

    sass plugin, config change

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from 'react';
+import style from './navigation.module.scss';
 
 const Navigation = () => {
   return (
     <nav>
       <ul>
         <li>
-          <a href="#">home</a>
+          <a href="/home" className={style.link}>home</a>
         </li>
         <li>
-          <a href="#">about</a>
+          <a href="/about">about</a>
         </li>
         <li>
-          <a href="#">projects</a>
+          <a href="/projects">projects</a>
         </li>
         <li>
-          <a href="#">contact</a>
+          <a href="/contact">contact</a>
         </li>
       </ul>
     </nav>
