bb8bb0c3432f244fe0a8b2929a564d8ab2112b6b
Milan <mistikone1@gmail.com>
2020-05-05 13:42:16 +0200

    added hero section

---
+++ src/components/hero/hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './hero.module.scss';
+
+const Hero = () => {
+  return (
+    <div className={style.hero}>Hello world!</div>
+  )
+}
+
+export default Hero
