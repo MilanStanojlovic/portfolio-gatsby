c1d88348e89f0ae2c6c14fdddbbe5cd897b857b3
Milan <mistikone1@gmail.com>
2020-05-07 13:41:30 +0200

    installed and imported playfair display and lato fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
+
 import Layout from '../components/layout/layout';
+
 import '../style/global.scss';
+import '../style/buttons.scss'; 
+//TODO: make only one import that imports all styles
+import 'typeface-lato';
+import 'typeface-playfair-display'
 
 const Index = () => {
   return (
