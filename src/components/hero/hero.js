66e9c6f0d274b648850846536bbfae46366a519a
Milan <mistikone1@gmail.com>
2020-05-07 13:40:13 +0200

    finished hero section

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -3,10 +3,16 @@ import style from './hero.module.scss';
 
 const Hero = () => {
   return (
-    <div className={[style.hero, 'section'].join(' ')}>
-      <span>Milan is a developer</span>
-      <h1>Helping businesses develop bespoke web solutions.</h1>
-      <button>Contact</button>
+    <div className={[style.hero]}>
+      <div className="inner-container">
+        <div className={style.hero__content}>
+          <div className="pitch">
+            <span className="heading">Milan is a developer</span>
+            <h1 className="heading"><span className="highlight-text">Helping businesses develop</span> bespoke web solutions.</h1>
+            <button className={[style.hero__button, 'button'].join(' ')}>Contact</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
