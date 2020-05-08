08850d064e78c4fd05b3e11463621c0d253e18e8
Milan <mistikone1@gmail.com>
2020-05-08 13:56:34 +0200

    about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import style from './about.module.scss';
 
 const About = () => {
   return (
-    <div className={[style.about, 'section'].join(' ')}>
-      About
+    <div className="section">
+      <div className="inner-container">
+        <div className="section__content">
+          <div>
+            <div className="heading">
+              About.
+            </div>
+            <p className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
