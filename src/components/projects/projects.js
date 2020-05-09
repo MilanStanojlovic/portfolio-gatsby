3e3c1d6eba90a904368a0758e6c39e36c6542786
Milan <mistikone1@gmail.com>
2020-05-09 15:48:10 +0200

    projects component changes

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import style from './projects.module.scss';
+import Project from '../project/project';
 
 const Projects = () => {
   return (
-    <div className={[style.projects, 'section'].join(' ')}>
-      Projects
+    <div className="section" id="projects">
+      <div className="inner-container">
+        <div className="section__content">
+          <div className={style.projects}>
+            <div className="heading">Projects.</div>
+            <Project />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
