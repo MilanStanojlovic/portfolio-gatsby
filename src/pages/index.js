814387f8a852aea07af57dfd3a687949626ce5a3
Milan <mistikone1@gmail.com>
2020-05-12 14:22:54 +0200

    style modifications

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 import Layout from '../components/layout/layout';
+import Head from '../components/head/head';
 
-import '../style/global.scss';
-import '../style/buttons.scss'; 
-//TODO: make only one import that imports all styles
+import '../style/index.scss';
 import 'typeface-lato';
-import 'typeface-playfair-display'
+import 'typeface-playfair-display';
 
 const Index = () => {
   return (
     <div>
+      <Head />
       <Layout />
     </div>
   )
