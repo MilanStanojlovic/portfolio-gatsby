9ad192af48d57ea2649a93493b78473c1a5b8294
Milan <mistikone1@gmail.com>
2020-05-15 15:37:40 +0200

    local projects.json file and fetching data over graphql

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import style from './project.module.scss';
 
-const Project = ({ even }) => {
+const Project = ({ even, title, image, imageAlt, description, technologies, github, web }) => {
   return (
     <div className={even ? style.projectFlip : style.project}>
-      <div className={even ? style.projectFlip__image : style.project__image}>img</div>
-      <div className={even ? style.projectFlip__title : style.project__title}>Title</div>
-      <div className={even ? style.projectFlip__description : style.project__description}>Descrip</div>
-      <div className={even ? style.projectFlip__technologies : style.project__technologies}>Tech</div>
-      <div className={even ? style.projectFlip__links : style.project__links}>github web</div>
+      <div className={even ? style.projectFlip__image : style.project__image}><img src={image} style={{ width: "100%" }} alt={imageAlt}></img></div>
+      <div className={even ? style.projectFlip__title : style.project__title}>{title}</div>
+      <div className={even ? style.projectFlip__description : style.project__description}>{description}</div>
+      <div className={even ? style.projectFlip__technologies : style.project__technologies}>{technologies}</div>
+      <div className={even ? style.projectFlip__links : style.project__links}>{github} {web}</div>
     </div>
   )
 }
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import style from './projects.module.scss';
 import Project from '../project/project';
+import { useStaticQuery, graphql } from 'gatsby'
 
 const Projects = () => {
+  const data = useStaticQuery(graphql`
+    query{
+      allProjectsJson {
+        edges {
+          node {
+            id
+            web
+            title
+            technologies
+            image
+            imageAlt
+            github
+            description
+          }
+        }
+      }
+    }
+  `)
+
+  const projects = data.allProjectsJson.edges.map((project, index) => {
+    return (
+      <Project key={project.node.id}
+        even={index}
+        title={project.node.title}
+        description={project.node.description}
+        image={project.node.image}
+        imageAlt={project.node.imageAlt}
+        technologies={project.node.technologies}
+        github={project.node.github}
+        web={project.node.web}
+      />
+    )
+  })
   return (
     <div id="projects">
       <div className="inner-container">
         <div>
           <div className={style.projects}>
             <div className="heading">Projects.</div>
-            <Project even={0 % 2 === 0} />
-            <Project even={1 % 2 === 0} />
-            <Project even={2 % 2 === 0} />
+            {/* <Project even={0 % 2 === 0} /> */}
+            {projects}
           </div>
         </div>
       </div>
