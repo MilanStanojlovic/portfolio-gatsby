8b881a39d21ea9053d2e452ea155f43e7ab0568d
Milan <mistikone1@gmail.com>
2020-05-18 16:39:15 +0200

    style and template changes

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -5,19 +5,28 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Project = ({ even, title, image, description, technologies, github, web }) => {
+
+  const tech = technologies.map((technology, index) => {
+    return <li className="text" key={index}>{technology}</li>
+  })
   return (
     <div className={even ? style.projectFlip : style.project}>
       <div className={even ? style.projectFlip__image : style.project__image} style={{ backgroundImage: `url(${image})` }}></div>
       <div className={even ? style.projectFlip__title : style.project__title}><h2 className="heading__secondary">{title}</h2></div>
       <div className={even ? style.projectFlip__description : style.project__description}><p className="text">{description}</p></div>
       <div className={even ? style.projectFlip__technologies : style.project__technologies}>
-        <ul>
-          <li className="text">Angular</li>
-          <li className="text">Scss</li>
-          <li className="text">Typescript</li>
+        <ul className={style.list}>
+          {tech}
         </ul>
       </div>
-      <div className={even ? style.projectFlip__links : style.project__links}><FontAwesomeIcon icon={faGithub} className="icon" size="2x" /> <FontAwesomeIcon icon={faExternalLinkAlt} size="2x" className={["icon", style.icon].join(' ')} /></div>
+      <div className={even ? style.projectFlip__links : style.project__links}>
+        <a href={github} aria-label={`${title} github`} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} className="icon" size="2x" />
+        </a>
+        <a href={web} aria-label={`${title} website`} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faExternalLinkAlt} size="2x" className="icon" />
+        </a>
+      </div>
     </div>
   )
 }
